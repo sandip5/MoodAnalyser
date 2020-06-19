48684571c6d89357107d8b29ae672b799db6d348
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-20 01:50:15 +0530

    ADDED- analyseMood method return SAD to pass Test Case

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -7,7 +7,7 @@ public class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a sad message");
+        String mood = moodAnalyser.analyseMood("I am in sad mood.");
         Assert.assertEquals( "SAD",mood);
     }
 
