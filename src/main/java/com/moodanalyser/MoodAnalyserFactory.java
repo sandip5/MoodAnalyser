39754d453d6f8cd36cc1216d03c09918c239d5b5
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-24 23:32:39 +0530

    ADDED- MoodAnalyserFactory Crated

---
+++ src/main/java/com/moodanalyser/MoodAnalyserFactory.java
@@ -0,0 +1,26 @@
+package com.moodanalyser;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class MoodAnalyserFactory {
+    public static MoodAnalyser createMoodAnalyser(String message) {
+        try {
+            Class<?> moodAnalyserClass = Class.forName("com.moodanalyser.MoodAnalyser");
+            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor(String.class);
+            Object moodObj = moodConstructor.newInstance(message);
+            return (MoodAnalyser) moodObj;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -3,37 +3,18 @@ package com.moodanalyser;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import static java.lang.Class.forName;
 
 public class MoodAnalyserTest {
     @Test
     public void givenMoodAnalyser_WhenProper_ShouldReturnObject() {
-        Constructor<?> constructor = null;
+        MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser("I am in happy mood");
         try {
-            constructor = forName("com.moodanalyser.MoodAnalyser").getConstructor(String.class);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            Object myObj = constructor.newInstance("I am in a happy mood.");
-            MoodAnalyser moodAnalyser = (MoodAnalyser) myObj;
             String mood = moodAnalyser.analyseMood();
-            Assert.assertEquals("HAPPY",mood);
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+            Assert.assertEquals("HAPPY", mood);
         } catch (MoodAnalyserException e) {
             e.printStackTrace();
         }
     }
-
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood.");
@@ -42,48 +23,48 @@ public class MoodAnalyserTest {
             exceptionRule.expect(MoodAnalyserException.class);
             String mood = moodAnalyser.analyseMood();
             Assert.assertEquals("SAD", mood);
-        }catch (MoodAnalyserException ignored){
+        } catch (MoodAnalyserException ignored) {
         }
     }
     @Test
     public void givenMessage_WhenNotSad_ShouldReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
-        try{
+        try {
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(MoodAnalyserException.class);
-            String mood =moodAnalyser.analyseMood();
-            Assert.assertEquals("HAPPY",mood);
-        }catch (MoodAnalyserException ignored){
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyserException ignored) {
         }
     }
     @Test
     public void givenNullMood_ShouldReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        try{
+        try {
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(MoodAnalyserException.class);
-            String mood =moodAnalyser.analyseMood();
-            Assert.assertEquals("HAPPY",mood);
-        }catch (MoodAnalyserException e){
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyserException e) {
             e.printStackTrace();
         }
     }
     @Test
     public void givenNullMood_ShouldThrowException() {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        try{
+        try {
             moodAnalyser.analyseMood();
-        }catch (MoodAnalyserException e){
-            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL,e.type);
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
         }
     }
     @Test
     public void givenEmptyMood_ShouldThrowException() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("");
-        try{
+        try {
             moodAnalyser.analyseMood();
-        }catch (MoodAnalyserException e){
-            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY,e.type);
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.type);
         }
     }
 }
